7eb7bc5247bd4cec6d8561f275220a50b37bbcf3
Abdullah Work <abedalluhnourden@gmail.com>
2026-08-08 06:59:55 +0300

    feat(ui,auth): implement auth UI from scratch

--- lib/modules/auth/view/screens/forget_passwd_screen.dart
+++ lib/modules/auth/view/screens/forget_passwd_screen.dart
@@ -1,23 +1,14 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:phone_numbers_parser/phone_numbers_parser.dart';
 import 'package:project_2/core/constants/app_colors.dart';
-import 'package:project_2/core/constants/app_periods.dart';
 import 'package:project_2/core/constants/app_sizes.dart';
-import 'package:project_2/core/services/snackbar_service.dart';
 import 'package:project_2/core/theme/app_theme.dart';
-import 'package:project_2/core/constants/app_shadow.dart';
-import 'package:project_2/core/validators/phone_validator.dart';
-import 'package:project_2/core/widgets/app_loading.dart';
-import 'package:project_2/core/widgets/custom_button.dart';
-import 'package:project_2/core/widgets/custom_textfield.dart';
 import 'package:project_2/core/widgets/pop_button.dart';
 import 'package:project_2/modules/auth/cubit/auth_cubit.dart';
 import 'package:project_2/modules/auth/view/widgets/forget_passwd_header.dart';
-import 'package:project_2/modules/auth/view/widgets/phone_number_prefix.dart';
-import 'package:project_2/modules/auth/view/widgets/section_title.dart';
+import 'package:project_2/modules/auth/view/widgets/forget_password_form.dart';
 
 class ForgetPasswdScreen extends StatefulWidget {
   const ForgetPasswdScreen({super.key});
@@ -39,14 +30,6 @@ class _ForgetPasswdScreenState extends State<ForgetPasswdScreen> {
     countryCode = ValueNotifier<String>('+963');
     isoCode = ValueNotifier<IsoCode>(IsoCode.SY);
     super.initState();
-  }
-
-  void test(BuildContext context, {required AuthCubit cubit}) {
-    if (!_forgetPasswdKey.currentState!.validate()) return;
-    FocusScope.of(context).unfocus();
-    cubit.forgetPasswd(
-      phoneNumber: countryCode.value + _phoneController.text.trim(),
-    );
   }
 
   @override
@@ -76,106 +59,38 @@ class _ForgetPasswdScreenState extends State<ForgetPasswdScreen> {
           statusBarIconBrightness: Brightness.dark,
         ),
       ),
-      body: BlocProvider<AuthCubit>(
-        create: (context) => AuthCubit(),
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: RadialGradient(
-              center: Alignment.topLeft,
-              radius: 1,
-              colors: <Color>[
-                AppColors.primary.withAlpha(80),
-                AppColors.primary.withAlpha(60),
-                AppColors.primary.withAlpha(0),
-              ],
-              stops: const <double>[0.0, 0.3, 1.0],
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            center: Alignment.topLeft,
+            radius: 1,
+            colors: <Color>[
+              AppColors.primary.withAlpha(80),
+              AppColors.primary.withAlpha(60),
+              AppColors.primary.withAlpha(0),
+            ],
+            stops: const <double>[0.0, 0.3, 1.0],
           ),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: SingleChildScrollView(
-              padding: EdgeInsets.symmetric(horizontal: AppSizes.paddingH),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  // Top Space
-                  SizedBox(height: AppSizes.xxxLargeSpace),
+        ),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: AppSizes.paddingH),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Top Space
+              SizedBox(height: AppSizes.xxxLargeSpace),
 
-                  const ForgetPasswdHeader(),
+              const ForgetPasswdHeader(),
 
-                  SizedBox(height: AppSizes.largeSpace),
+              SizedBox(height: AppSizes.largeSpace),
 
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: AppSizes.paddingH,
-                      vertical: AppSizes.paddingV,
-                    ),
-                    decoration: BoxDecoration(
-                      color: AppColors.white,
-                      borderRadius: BorderRadius.circular(AppSizes.radius),
-                      boxShadow: AppShadow.cardShadow,
-                    ),
-                    child: Form(
-                      key: _forgetPasswdKey,
-                      child: BlocConsumer<AuthCubit, AuthState>(
-                        listener: (context, state) {
-                          if (state is ForgetPasswdError) {
-                            snackBarService.showError(message: state.message);
-                          } else if (state is ForgetPasswdSuccess) {
-                            snackBarService.showSuccess(message: 'OTP Sent');
-                          }
-                        },
-                        builder: (context, state) {
-                          final AuthCubit cubit = context.read<AuthCubit>();
-                          return Column(
-                            children: <Widget>[
-                              FadeInLeft(
-                                delay: AppPeriods.animationDelay(4),
-                                child: const SectionTitle(
-                                  title: 'Phone Number',
-                                ),
-                              ),
-                              FadeInLeft(
-                                delay: AppPeriods.animationDelay(5),
-                                child: CustomTextField(
-                                  controller: _phoneController,
-                                  hintText: '9XX XXX XXX',
-                                  radius: AppSizes.radius,
-                                  keyboardType: TextInputType.phone,
-                                  validator: (value) => PhoneValidator.validate(
-                                    value,
-                                    country: isoCode.value,
-                                  ),
-                                  prefixIcon: PhoneNumberPrefix(
-                                    countryCode: countryCode,
-                                    isoCode: isoCode,
-                                  ),
-                                  onEditingComplete: () =>
-                                      test(context, cubit: cubit),
-                                ),
-                              ),
-
-                              SizedBox(height: AppSizes.largeSpace),
-                              if (state is ForgetPasswdLoading)
-                                const AppLoading()
-                              else
-                                FadeIn(
-                                  delay: AppPeriods.animationDelay(6),
-                                  child: CustomButton(
-                                    label: 'Send OTP',
-                                    onPressed: () =>
-                                        test(context, cubit: cubit),
-                                  ),
-                                ),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ],
+              ForgetPasswordForm(
+                forgetPasswdKey: _forgetPasswdKey,
+                phoneController: _phoneController,
+                isoCode: isoCode,
+                countryCode: countryCode,
               ),
-            ),
+            ],
           ),
         ),
       ),
---
+++ lib/modules/auth/view/widgets/forget_password_form.dart
@@ -0,0 +1,137 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:phone_numbers_parser/phone_numbers_parser.dart';
+import 'package:project_2/core/constants/app_colors.dart';
+import 'package:project_2/core/constants/app_periods.dart';
+import 'package:project_2/core/constants/app_shadow.dart';
+import 'package:project_2/core/constants/app_sizes.dart';
+import 'package:project_2/core/routing/app_routes.dart';
+import 'package:project_2/core/services/snackbar_service.dart';
+import 'package:project_2/core/validators/phone_validator.dart';
+import 'package:project_2/core/widgets/app_loading.dart';
+import 'package:project_2/core/widgets/custom_button.dart';
+import 'package:project_2/core/widgets/custom_textfield.dart';
+import 'package:project_2/modules/auth/cubit/auth_cubit.dart';
+import 'package:project_2/modules/auth/view/widgets/phone_number_prefix.dart';
+import 'package:project_2/modules/auth/view/widgets/section_title.dart';
+
+class ForgetPasswordForm extends StatefulWidget {
+  const ForgetPasswordForm({
+    super.key,
+    required GlobalKey<FormState> forgetPasswdKey,
+    required TextEditingController phoneController,
+    required this.isoCode,
+    required this.countryCode,
+  }) : _forgetPasswdKey = forgetPasswdKey,
+       _phoneController = phoneController;
+
+  final GlobalKey<FormState> _forgetPasswdKey;
+  final TextEditingController _phoneController;
+  final ValueNotifier<IsoCode> isoCode;
+  final ValueNotifier<String> countryCode;
+
+  @override
+  State<ForgetPasswordForm> createState() => _ForgetPasswordFormState();
+}
+
+class _ForgetPasswordFormState extends State<ForgetPasswordForm> {
+  late final AuthCubit cubit;
+
+  @override
+  void initState() {
+    cubit = AuthCubit();
+    super.initState();
+  }
+
+  void handleForgetPasswd(BuildContext context, {required AuthCubit cubit}) {
+    if (!widget._forgetPasswdKey.currentState!.validate()) return;
+    FocusScope.of(context).unfocus();
+    cubit.forgetPasswd(
+      phoneNumber:
+          widget.countryCode.value + widget._phoneController.text.trim(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: AppSizes.paddingH,
+        vertical: AppSizes.paddingV,
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(AppSizes.radius),
+        boxShadow: AppShadow.cardShadow,
+      ),
+      child: Form(
+        key: widget._forgetPasswdKey,
+        child: BlocProvider<AuthCubit>.value(
+          value: cubit,
+          child: BlocListener<AuthCubit, AuthState>(
+            listener: (context, state) {
+              if (state is ForgetPasswdError) {
+                snackBarService.showError(message: state.message);
+              } else if (state is ForgetPasswdSuccess) {
+                snackBarService.showSuccess(message: 'OTP Sent');
+                AppRoutes.toOtpScreen();
+              }
+            },
+
+            child: Column(
+              children: <Widget>[
+                FadeInLeft(
+                  delay: AppPeriods.animationDelay(4),
+                  child: const SectionTitle(title: 'Phone Number'),
+                ),
+                Builder(
+                  builder: (context) {
+                    return FadeInLeft(
+                      delay: AppPeriods.animationDelay(5),
+                      child: CustomTextField(
+                        controller: widget._phoneController,
+                        hintText: '9XX XXX XXX',
+                        radius: AppSizes.radius,
+                        keyboardType: TextInputType.phone,
+                        validator: (value) => PhoneValidator.validate(
+                          value,
+                          country: widget.isoCode.value,
+                        ),
+                        prefixIcon: PhoneNumberPrefix(
+                          countryCode: widget.countryCode,
+                          isoCode: widget.isoCode,
+                        ),
+                        onEditingComplete: () => handleForgetPasswd(
+                          context,
+                          cubit: context.read<AuthCubit>(),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+
+                SizedBox(height: AppSizes.largeSpace),
+
+                BlocBuilder<AuthCubit, AuthState>(
+                  builder: (context, state) {
+                    final AuthCubit cubit = context.read<AuthCubit>();
+                    if (state is ForgetPasswdLoading) return const AppLoading();
+                    return FadeInLeft(
+                      delay: AppPeriods.animationDelay(6),
+                      child: CustomButton(
+                        label: 'Send OTP',
+                        onPressed: () =>
+                            handleForgetPasswd(context, cubit: cubit),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
